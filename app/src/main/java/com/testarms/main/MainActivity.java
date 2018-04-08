6534f813a045e4bd47522f8c4a14500e14d215f7
okman999@aliyun.com <1234>
2018-04-08 11:13:51 +0800

    Initial commit

---
+++ app/src/main/java/com/testarms/main/MainActivity.java
@@ -0,0 +1,14 @@
+package com.testarms.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.testarms.R;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
